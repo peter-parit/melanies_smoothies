883e8456ea4b6eeca8d76281047ac69a8c85058e
peterparit <144830000+peter-parit@users.noreply.github.com>
2026-05-20 17:39:22 +0700

    Add 'search_on' column to fruit options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 
 # get table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name')).select(col('search_on'))
 
 # display the dataframe
 # st.dataframe(data=my_dataframe, use_container_width=True)
