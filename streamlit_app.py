c6fc0f833b737e4ba7c63e06e5b486c0745bb8b3
peterparit <144830000+peter-parit@users.noreply.github.com>
2026-05-20 17:42:55 +0700

    Add Pandas conversion for smoothie options display
    Convert Snowflake DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -21,7 +22,12 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 
 # display the dataframe
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# convert to pandas
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
