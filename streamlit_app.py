4e2f093299e611a6241434b7b5991377d56f564a
peterparit <144830000+peter-parit@users.noreply.github.com>
2026-05-20 17:40:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 
 # display the dataframe
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -34,7 +35,7 @@ if ingredients_list:
     ingredients_str = ''
     for ingredient in ingredients_list:
         ingredients_str += ingredient + ' '
-        st.subheader(ingredient + 'Nutrition Information')
+        st.subheader(ingredient + ' Nutrition Information')
         smoothiefruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredient)
         sf_df = st.dataframe(data = smoothiefruit_response.json(), use_container_width = True)
 
